5b63003c19995bf59efc970e731e30025a3d82c0
Anahi Salgado <anahi@platzi.com>
2018-12-06 11:55:02 -0500

    Apilando Textos

--- platzi_trips_app/lib/review.dart
+++ platzi_trips_app/lib/review.dart
@@ -3,12 +3,78 @@ import 'package:flutter/material.dart';
 class Review extends StatelessWidget {
 
   String pathImage = "asset/img/people.jpg";
+  String name = "Varuna Yasas";
+  String details = "1 review 5 photos";
+  String comment = "There is an amazing place in Sri Lanka";
 
-  Review(this.pathImage);
+  Review(this.pathImage, this.name, this.details, this.comment);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
+
+    final userComment = Container(
+      margin: EdgeInsets.only(
+          left: 20.0
+      ),
+
+      child: Text(
+        details,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+            fontFamily: "Lato",
+            fontSize: 13.0,
+            fontWeight: FontWeight.w900
+        ),
+
+      ),
+
+    );
+
+    final userInfo = Container(
+      margin: EdgeInsets.only(
+        left: 20.0
+    ),
+
+      child: Text(
+        details,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+            fontFamily: "Lato",
+            fontSize: 13.0,
+          color: Color(0xFFa3a5a7)
+        ),
+
+      ),
+
+    );
+
+    final userName = Container(
+      margin: EdgeInsets.only(
+        left: 20.0
+      ),
+
+      child: Text(
+        name,
+        textAlign: TextAlign.left,
+        style: TextStyle(
+          fontFamily: "Lato",
+            fontSize: 17.0
+        ),
+
+      ),
+
+    );
+
+    final userDetails = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        userName,
+        userInfo,
+        userComment
+
+      ],
+    );
 
     final photo = Container (
       margin: EdgeInsets.only(
@@ -31,6 +97,8 @@ class Review extends StatelessWidget {
 
     return Row (
       children: <Widget>[
+        photo,
+        userComment
 
       ],
     );
---
+++ platzi_trips_app/lib/review_list.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ReviewList extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return null;
+  }
+
+}
