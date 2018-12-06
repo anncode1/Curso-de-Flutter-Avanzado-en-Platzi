d2032e2b6513a8189571c1d1956917d3cb901e9a
Anahi Salgado <anahi@platzi.com>
2018-12-06 11:23:22 -0500

    Tipografia

--- platzi_trips_app/lib/description_place.dart
+++ platzi_trips_app/lib/description_place.dart
@@ -62,6 +62,7 @@ class DescriptionPlace extends StatelessWidget {
           child: Text(
             namePlace,
             style: TextStyle(
+              fontFamily: "Lato",
               fontSize: 30.0,
               fontWeight: FontWeight.w900
             ),
@@ -94,6 +95,7 @@ class DescriptionPlace extends StatelessWidget {
       child: new Text(
         descriptionPlace,
         style: const TextStyle(
+            fontFamily: "Lato",
             fontSize: 16.0,
             fontWeight: FontWeight.bold,
             color: Color(0xFF56575a)
