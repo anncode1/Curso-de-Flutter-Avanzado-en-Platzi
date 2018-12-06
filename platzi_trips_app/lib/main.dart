ae6567fd3cc6509752b0900140cfab25340b423a
Anahi Salgado <anahi@platzi.com>
2018-12-06 09:57:06 -0500

    Hola Mundo

--- platzi_trips_app/lib/main.dart
+++ platzi_trips_app/lib/main.dart
@@ -20,7 +20,14 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("Hola Mundo Feliz"),
+        ),
+        body: Center(
+          child: Text("Hola Mundo Platzi"),
+        ),
+      )//MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
