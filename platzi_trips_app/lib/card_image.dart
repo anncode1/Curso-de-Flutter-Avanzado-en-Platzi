cf22941494bb4a6e561aba8b06a00a11023469f4
Anahi Salgado <anahi@platzi.com>
2018-12-07 10:00:58 -0500

    Widget Floating Action Button

--- platzi_trips_app/lib/card_image.dart
+++ platzi_trips_app/lib/card_image.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'floating_action_button_green.dart';
 
 class  CardImage extends StatelessWidget {
 
@@ -37,7 +38,13 @@ class  CardImage extends StatelessWidget {
       ),
     );
 
-    return card;
+    return Stack(
+      alignment: Alignment(0.9,1.1),
+      children: <Widget>[
+        card,
+        FloatingActionButtonGreen()
+      ],
+    );
   }
 
 }
