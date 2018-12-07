d335634e00279974d84fff2ce1b6f71c931d506f
Anahi Salgado <anahi@platzi.com>
2018-12-07 09:35:02 -0500

    Button InkWell

--- platzi_trips_app/lib/description_place.dart
+++ platzi_trips_app/lib/description_place.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'button_purple.dart';
 
 class DescriptionPlace extends StatelessWidget {
 
@@ -105,9 +106,11 @@ class DescriptionPlace extends StatelessWidget {
     );
 
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
         title_stars,
-        description
+        description,
+        ButtonPurple("Navigate")
       ],
     );
 
