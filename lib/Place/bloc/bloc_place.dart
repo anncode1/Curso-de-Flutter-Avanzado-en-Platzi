4dc699c86b2af9d762ae34eb928e2cadcbb1e91e
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-04-22 12:07:01 -0500

    Integreando libreria providers y Exponiendo BLoCs

---
+++ lib/Place/bloc/bloc_place.dart
@@ -0,0 +1,9 @@
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+
+class PaceBloc implements Bloc {
+
+  @override
+  void dispose() {
+
+  }
+}
---
+++ lib/User/bloc/bloc_user.dart
@@ -0,0 +1,9 @@
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+
+class UserBloc implements Bloc {
+
+  @override
+  void dispose() {
+
+  }
+}
