f3ae67f9056470684487b0e386a42f15e37b7424
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-04-23 11:32:33 -0500

    Generic Button SignIn Created

--- lib/User/ui/screens/sign_in_screen.dart
+++ lib/User/ui/screens/sign_in_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:platzi_trips_app/widgets/gradient_back.dart';
+import 'package:platzi_trips_app/widgets/button_green.dart';
 
 class SignInScreen extends StatefulWidget {
 
@@ -25,6 +26,7 @@ class _SignInScreen extends State<SignInScreen> {
         children: <Widget>[
           GradientBack("", null),
           Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Text("Welcome \n This is your Travel App",
                 style: TextStyle(
@@ -35,6 +37,13 @@ class _SignInScreen extends State<SignInScreen> {
 
 
                 ),
+              ),
+              ButtonGreen(text: "Login with Gmail",
+                  onPressed: () {
+
+                },
+                width: 300.0,
+                height: 50.0,
               )
 
             ],
