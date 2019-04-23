0ac43c4b13cc9af203529b7a973540ed296bee6f
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-04-23 12:04:15 -0500

    Repository File of BLoC pattern implemented

---
+++ lib/User/repository/firebase_auth_api.dart
@@ -0,0 +1,20 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class FirebaseAuthAPI {
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  Future<FirebaseUser> signIn() async {
+    GoogleSignInAccount googleSignInAccount = await googleSignIn.signIn();
+    GoogleSignInAuthentication gSA = await googleSignInAccount.authentication;
+
+    FirebaseUser user = await _auth.signInWithCredential(
+        GoogleAuthProvider.getCredential(idToken: gSA.idToken, accessToken: gSA.accessToken));
+
+    return user;
+
+  }
+
+}
