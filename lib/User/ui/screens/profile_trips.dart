c414b414afd854e5f346579932c3d56388c32d5f
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-05-03 15:10:41 -0500

    Logged User Persist

--- lib/User/ui/screens/profile_trips.dart
+++ lib/User/ui/screens/profile_trips.dart
@@ -1,27 +1,90 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
+import 'package:platzi_trips_app/User/model/user.dart';
 import 'package:platzi_trips_app/User/ui/screens/profile_header.dart';
 import 'package:platzi_trips_app/User/ui/widgets/profile_places_list.dart';
 import 'package:platzi_trips_app/User/ui/widgets/profile_background.dart';
 
 class ProfileTrips extends StatelessWidget {
+  UserBloc userBloc;
+
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    /*return Container(
-      color: Colors.indigo,
-    );*/
-    return Stack(
+    userBloc = BlocProvider.of<UserBloc>(context);
+
+    return StreamBuilder(
+      stream: userBloc.authStatus,
+      builder: (BuildContext context, AsyncSnapshot snapshot){
+        switch (snapshot.connectionState){
+          case ConnectionState.waiting:
+          case ConnectionState.none:
+
+          case ConnectionState.active:
+          case ConnectionState.done:
+
+          default:
+
+        }
+      },
+    );
+
+
+
+
+
+      /*Stack(
       children: <Widget>[
         ProfileBackground(),
         ListView(
           children: <Widget>[
-            ProfileHeader(),
-            ProfilePlacesList()
+            ProfileHeader(),//User datos
+            ProfilePlacesList()//User uid
 
           ],
         ),
       ],
-    );
+    );*/
+  }
+
+  Widget showProfileData(AsyncSnapshot snapshot){
+    if(!snapshot.hasData || snapshot.hasError){
+      print("No logeado");
+      return Stack(
+        children: <Widget>[
+          ProfileBackground(),
+          ListView(
+            children: <Widget>[
+              Text("Usuario no logeado. Haz Login")
+
+            ],
+          ),
+        ],
+      );
+    }else {
+      print("Logeado");
+      var user = User(
+          uid: snapshot.data.uid,
+          name: snapshot.data.displayName,
+          email: snapshot.data.email,
+          photoURL: snapshot.data.photoUrl
+      );
+
+      return Stack(
+        children: <Widget>[
+          ProfileBackground(),
+          ListView(
+            children: <Widget>[
+              ProfileHeader(),//User datos
+              ProfilePlacesList()//User uid
+
+            ],
+          ),
+        ],
+      );
+    }
+
   }
 
 }
