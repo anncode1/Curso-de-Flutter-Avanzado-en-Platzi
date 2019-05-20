3dd4f2a75a319680190f401de075489da739e22d
Anahi Salgado <anahisalgado.diaz@gmail.com>
2019-05-19 21:45:14 -0500

    Toggle Button Like

--- lib/Place/ui/screens/header_appbar.dart
+++ lib/Place/ui/screens/header_appbar.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:generic_bloc_provider/generic_bloc_provider.dart';
+import 'package:platzi_trips_app/User/bloc/bloc_user.dart';
+import 'package:platzi_trips_app/User/model/user.dart';
 import 'package:platzi_trips_app/widgets/gradient_back.dart';
 import 'package:platzi_trips_app/Place/ui/widgets/card_image_list.dart';
 
@@ -6,12 +9,55 @@ class HeaderAppBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Stack(
-      children: <Widget>[
-        GradientBack(height: 250.0),
-        CardImageList()
-      ],
+    UserBloc userBloc;
+    userBloc = BlocProvider.of<UserBloc>(context);
+
+    return StreamBuilder(
+      stream: userBloc.authStatus,
+      builder: (BuildContext context, AsyncSnapshot snapshot){
+        switch (snapshot.connectionState){
+          case ConnectionState.waiting:
+            return Center(child: CircularProgressIndicator(),);
+          case ConnectionState.none:
+            return Center(child: CircularProgressIndicator(),);
+          case ConnectionState.active:
+            return showPlacesData(snapshot);
+          case ConnectionState.done:
+            return showPlacesData(snapshot);
+          default:
+            return showPlacesData(snapshot);
+        }
+      },
     );
+
+
+
+
   }
 
+
+  Widget showPlacesData(AsyncSnapshot snapshot){
+    if(!snapshot.hasData || snapshot.hasError){
+      return Stack(
+        children: <Widget>[
+          GradientBack(height: 250.0),
+          Text("Usuario no logeado. Haz Login")
+        ],
+      );
+    } else {
+      User user = User(
+          uid: snapshot.data.uid,
+          name: snapshot.data.displayName,
+          email: snapshot.data.email,
+          photoURL: snapshot.data.photoUrl
+      );
+
+      return Stack(
+        children: <Widget>[
+          GradientBack(height: 250.0),
+          CardImageList(user)
+        ],
+      );
+    }
+  }
 }
